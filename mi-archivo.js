0a08be54fb72bf3cb0525bcd37f863684847d319
crisan8246 <malgon8246.cm@gmail.com>
2021-06-21 22:14:43 -0500

    se agrega js

---
+++ mi-archivo.js
@@ -0,0 +1,23 @@
+//variable*/
+const miNombre = "Cristian";
+const apellido = "Malagon";
+const numero = 16790;
+//const espacio = " ";
+const edad = parseInt(prompt("Ingrese su edad"));
+alert("Eres muy joven " + edad);
+
+//suma*/
+const concatenacion = miNombre +" " + apellido +" " + numero +" " + edad;
+
+//validar en consola*/
+console.log(concatenacion);
+
+//saludo*/
+const nombre = prompt("ingrese su nombre");
+alert("¡Hola!, te deseamos lo mejor " + nombre);
+
+
+
+
+
+
