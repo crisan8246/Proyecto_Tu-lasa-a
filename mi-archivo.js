76ad16fbebbe068d4659e2ed3d17bda3cc7f7877
crisan8246 <malgon8246.cm@gmail.com>
2021-06-23 19:40:50 -0500

    incluyendo condicional

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,20 +1,50 @@
+//clase 1
 //variable*/
-const miNombre = "Cristian";
-const apellido = "Malagon";
-const numero = 16790;
+//const miNombre = "Cristian";
+//const apellido = "Malagon";
+//const numero = 16790;
 //const espacio = " ";
-const edad = parseInt(prompt("Ingrese su edad"));
-alert("Eres muy joven " + edad);
+//const edad = parseInt(prompt("Ingrese su edad"));
+//alert("Eres muy joven " + edad);
 
 //suma*/
-const concatenacion = miNombre +" " + apellido +" " + numero +" " + edad;
+//const concatenacion = miNombre +" " + apellido +" " + numero +" " + edad;
 
 //validar en consola*/
-console.log(concatenacion);
+//console.log(concatenacion);
 
 //saludo*/
-const nombre = prompt("ingrese su nombre");
-alert("¡Hola!, te deseamos lo mejor " + nombre);
+//const nombre = prompt("ingrese su nombre");
+//alert("¡Hola!, te deseamos lo mejor " + nombre);
+
+//clase 2
+const car = "carne";
+const poll = "pollo";
+const cer = "cerdo"
+const nombre = prompt("ingresa el sabor de tu lasaña; carne, pollo, cerdo");
+ if(nombre == car){
+   alert("tu lasaña de carne ya se esta preparando")
+}
+else if(nombre == poll){
+   alert("tu lasaña de pollo ya se esta preparando");
+}
+else if(nombre == cer){
+    alert("tu lasaña de cerdo ya se esta preparando")
+}
+
+
+const numero = parseInt(prompt("ingrese un numero mayor o menor de 100") );
+if (numero >= 100){
+   alert("numero ingresado es correcto");
+}
+else if (numero <=100){
+   console.log("el numero es correcto"); 
+}
+
+
+
+
+
 
 
 
