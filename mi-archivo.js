831b0fa56704db98d4bdb7391681e92a30a5a323
crisan8246 <malgon8246.cm@gmail.com>
2021-06-27 21:03:34 -0500

    agrego turnos a mi proyecto

--- mi-archivo.js
+++ mi-archivo.js
@@ -18,28 +18,49 @@
 //alert("¡Hola!, te deseamos lo mejor " + nombre);
 
 //clase 2
-const car = "carne";
-const poll = "pollo";
-const cer = "cerdo"
-const nombre = prompt("ingresa el sabor de tu lasaña; carne, pollo, cerdo");
- if(nombre == car){
-   alert("tu lasaña de carne ya se esta preparando")
-}
-else if(nombre == poll){
-   alert("tu lasaña de pollo ya se esta preparando");
-}
-else if(nombre == cer){
-    alert("tu lasaña de cerdo ya se esta preparando")
+
+//const car = "carne";
+//const poll = "pollo";
+//const cer = "cerdo"
+//const nombre = prompt("ingresa el sabor de tu lasaña; carne, pollo, cerdo");
+ //if(nombre == car){
+   //alert("tu lasaña de carne ya se esta preparando")}
+//else if(nombre == poll){
+  // alert("tu lasaña de pollo ya se esta preparando");}
+//else if(nombre == cer){
+  //  alert("tu lasaña de cerdo ya se esta preparando")}
+
+//const numero = parseInt(prompt("ingrese un numero mayor o menor de 100") );
+//if (numero >= 100){
+  // alert("numero ingresado es correcto");
+//}
+//else if (numero <=100){
+ //  console.log("el numero es correcto"); }
+
+
+//clase 3
+
+//let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+// En cada repetición, calculamos el número ingresado x el número de repetición 
+//for (let i = 1; i <= 10; i++) 
+//{let resultado = ingresarNumero * i ;console.log(ingresarNumero +" x "+ i +" = "+ resultado);}
+
+
+for (let i = 1; i <= 5; i++) {
+   // En cada repetición solicitamos un nombre.
+   let ingresarNombre = prompt("Ingresar nombre");
+   // Informamos el turno asignado usando el número de repetición (i).
+   console.log(" Turno  N° "+i+" Nombre: "+ingresarNombre);
+     if (ingresarNombre == ""){
+      alert("No ingreso nombre"); }
+   else if( ingresarNombre){
+      alert(" Turno  N° "+i+" Nombre: "+ingresarNombre);
+   }
 }
 
 
-const numero = parseInt(prompt("ingrese un numero mayor o menor de 100") );
-if (numero >= 100){
-   alert("numero ingresado es correcto");
-}
-else if (numero <=100){
-   console.log("el numero es correcto"); 
-}
+
+
 
 
 
